fb38ac7575d6d0d13f29c5924ce136b4e2d73329
zhanyan <zhanyan.work@outlook.com>
2024-10-31 13:34:28 +0800

    init

---
+++ src/test/java/com/ownlin/z2o/Z2oApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownlin.z2o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z2oApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
