e1e1c318ae099224622374be963c85dc2c43573a
zhanyan <zhanyan.work@outlook.com>
2024-10-31 14:15:33 +0800

    添加graceful-response依赖并配置

--- src/main/java/com/ownlin/z2o/Z2oApplication.java
+++ src/main/java/com/ownlin/z2o/Z2oApplication.java
@@ -1,9 +1,10 @@
 package com.ownlin.z2o;
 
+import com.feiniaojin.gracefulresponse.EnableGracefulResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableGracefulResponse
 @SpringBootApplication
 public class Z2oApplication {
 
